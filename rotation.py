b254954c13b6c062415b872774629d42748ba50a
zarbazan <kiryandx@yandex.ru>
2021-11-09 22:13:35 +0300

    qweert2

--- rotation.py
+++ rotation.py
@@ -23,25 +23,25 @@ dt = 0.01          # шаг интегрирования
 Tend = 10         # время моделирования
 t0 = 0             # начало моделирования
 t = t0             # время
-Tcmd = 10           # целевая тяга
-Lim1 = 100000        # ограничение по скорости
-Lim2 = 100000        # ограничение по ускорению
+Tcmd = 100           # целевая тяга
+Lim1 = 500        # ограничение по скорости
+Lim2 = 500        # ограничение по ускорению
 
-kP = 2
-kI = 1
-kD = 0.5
+kP = 300
+kI = 10
+kD = 5
 
 kP1 = 2
 kI1 = 1
 kD1 = 0.5
 
-e_teta_past = teta
-e_teta_1_past = teta1
+e_teta_past = 0
+e_teta_1_past = 0
 
 
-def integral(param):
-    param +=param * dt
-    return param
+ #def integral(param):
+ #   param +=param * dt
+ #   return param
 
 # расчет ошибки
 def error(input1, input2):
@@ -50,9 +50,10 @@ def error(input1, input2):
 
 # PID вычислитель
 def PID(error, Kp, Ki, Kd, error_past, Lim):
-
+    integral = 0
     P = Kp * error
-    I = Ki * integral(error)
+    integral += error * dt
+    I = Ki * integral
     D = Kd * ((error - error_past)/dt)
     PID = P + I+ D
 
@@ -61,7 +62,7 @@ def PID(error, Kp, Ki, Kd, error_past, Lim):
     elif (PID < -Lim):
         PID = -Lim
 
-    #print("P", P, "I", I, "D", D)
+    print("P", P, "I", I, "D", D)
 
     return PID
 
@@ -79,22 +80,22 @@ def model(teta_2_cmd, Tcmd, kb, l, Iy):
 
 
 
-while t <= 2*dt: #Tend:
+while t <= Tend:
 
 
     e_teta = error(teta_cmd, teta)               # расчет ошибки по положению
-    e_teta_past = e_teta
     teta_1_cmd = PID(e_teta, kP, kI, kD, e_teta_past, Lim1)  # расчет  целевой скорости
     print(e_teta, e_teta_past, teta_1_cmd)
+    e_teta_past = e_teta
 
     e_teta_1 = error(teta_1_cmd, teta1)   # расчет ошибки по скорости
-    e_teta_1_past = e_teta_1
     teta_2_cmd = PID(e_teta_1, kP1, kI1, kD1, e_teta_1_past, Lim2)   # расчет  целевого ускорения
-    print(e_teta_1, e_teta_1_past, teta_2_cmd)
+    #print(e_teta_1, e_teta_1_past, teta_2_cmd)
+    e_teta_1_past = e_teta_1
 
     teta2 = model(teta_2_cmd, Tcmd, kb, l, Iy)
-    teta1 = integral(teta2)
-    teta = integral(teta1)
+    teta1 += teta2 * dt
+    teta += teta1 * dt
 
 
 
--- еуые.py
+++ еуые.py
@@ -3,7 +3,7 @@ a = 1
 c = 2
 
 def test(b, d):
-    b+=b
+    g+=b
     d=d**2
     return b, d
 
